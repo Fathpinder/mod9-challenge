8f55d1a6490f87537ca24b937c0d2c6398616925
markg <mark.grangaard@gmail.com>
2022-08-29 12:46:51 -0500

    updating with current progress

--- index.js
+++ index.js
@@ -1,14 +1,89 @@
 // TODO: Include packages needed for this application
-const inquirer = require('inquirer');
-const fs = require('fs');
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "title",
+      message: "Enter a project title",
+      validate: (nameInput) => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log("Please enter a title");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "Enter project description",
+    },
+    {
+      type: "input",
+      name: "installation",
+      message: "Enter installation instructions",
+    },
+    {
+      type: "input",
+      name: "usage",
+      message: "Enter usage information",
+    },
+    {
+      type: "input",
+      name: "credits",
+      message: "Enter credits",
+    },
+    {
+      type: "input",
+      name: "guidelines",
+      message: "Enter contribution guidelines",
+    },
+    {
+      type: "input",
+      name: "test",
+      message: "Enter test instructions",
+    },
+    {
+      type: "checkbox",
+      name: "licenses",
+      message: "What license should be used for this project?",
+      choices: ["BSD", "MIT", "GPL", "None"],
+    },
+    {
+      type: "input",
+      name: "github",
+      message: "Enter your GitHub username",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "Enter your email address",
+    },
+  ]);
+};
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+    if (err) {
+      return console.log(err);
+    }
+    console.log("Your README.md file has been created");
+  });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer.prompt(questions).then((data) => {
+    writeToFile("README.md", generateMarkdown(data));
+  });
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,11 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (!license) {
+    return "";
+  } else if (license === "BSD") {
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -14,6 +19,37 @@ function renderLicenseSection(license) {}
 function generateMarkdown(data) {
   return `# ${data.title}
 
+## Description: ${data.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [Contribution](#contribution)
+- [Tests](#tests)
+- [License](#license)
+- [Questions](#questions)
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## Credits
+${data.credits}
+
+## Contribution
+${data.guidelines}
+
+## Tests
+${data.test}
+
+## License
+
+## Questions
+- ${data.email} (preferred method)
+- https://github.com/${data.github}
 `;
 }
 
